d9430f609b0edfb839ace997f69c654fe9d9d5f9
Gokhan Ozdemir <gokhanworkacc@gmail.com>
2024-03-05 13:04:21 +0300

    I have started to create a function for calculate the ranks of the horses, it will be used in the game.

---
+++ race-game.js
@@ -0,0 +1,21 @@
+const rankCalculator = (race, age, berhaviour) => {
+
+//weighted values
+    let race;
+    let speed;
+    let age;
+    let behaviour;
+
+//weight factors
+//race 1.15, 0.85
+//age 0.75, 1.10
+//behaviour 0.65, 1.20
+
+
+}
+
+
+
+
+
+export default rankCalculator;
